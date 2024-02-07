f7ddab9ceb6cfe077c32fc9898e63519dcb619cb
shubham-369 <shubham.srivastav666@gmail.com>
2024-02-08 00:35:37 +0530

    code optimized

--- text_analysis.py
+++ text_analysis.py
@@ -10,33 +10,27 @@ def read_txt_file(file_path):
         print(f"Error reading file: {e}")
         return None
     
-file_path = './test.txt'
-# content = read_txt_file(file_path)
-# if content:
-#     print('Content read successfully!')
-#     print(content)
 
 def analyze_text(text):
     # Count words
     word_count = len(text.split())
 
     # Count lines
-    line_count = text.count('\n') + 1  # Add 1 for the last line
+    line_count = text.count('\n') + 1  
 
     # Count characters
     char_count = len(text)
 
     return word_count, line_count, char_count
 
-# Read content from the text file
-file_path = "./test.txt"  # Replace 'sample.txt' with the actual path to your text file
-content = read_txt_file(file_path)  # Assuming you have already defined the read_text_file function
-
+file_path = './test.txt'
+content = read_txt_file(file_path)
 if content:
-    # Analyze the text
+    print('Content read successfully!')
+    print(content)
+
     word_count, line_count, char_count = analyze_text(content)
 
-    # Print the results
     print("Text Analysis:")
     print(f"Number of words: {word_count}")
     print(f"Number of lines: {line_count}")
