918ea12ed76091a29492bc6d521fffc7e831a21d
shubham-369 <shubham.srivastav666@gmail.com>
2024-02-08 00:20:43 +0530

    First Commit

---
+++ text_analysis.py
@@ -0,0 +1,17 @@
+def read_txt_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+            return file_content
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+        return None
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        return None
+    
+file_path = './test.txt'
+content = read_txt_file(file_path)
+if content:
+    print('Content read successfully!')
+    print(content)
